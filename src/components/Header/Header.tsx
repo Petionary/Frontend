72d5d9b87e3aa22720a56dff663f9be6c91b6763
cos4338@gmail.com <cos4338@gmail.com>
2023-07-14 18:08:28 +0900

    design : apply design to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,31 +1,31 @@
 import Input from '../Input/Input';
 import { ReactComponent as MenuToggle } from '../../assets/MenuToggle.svg';
-import { ReactComponent as MessageIcon } from '../../assets/MessageIcon.svg';
-import { ReactComponent as UserIcon } from '../../assets/UserIcon.svg';
-import { ReactComponent as LogoutIcon } from '../../assets/LogoutIcon.svg';
+import Icons from './components/Icons';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const nav = useNavigate();
   return (
     <header className="flex justify-between px-10 w-full h-[4rem]">
       <section className="flex items-center w-[10rem] justify-between">
-        <span>
-          <MenuToggle className="cursor-pointer" onClick={() => alert('click')} />
-        </span>
-        <div className="font-bold bg-primary-dark py-2 px-3 text-white">PETIONARY.</div>
+        <MenuToggle
+          className="cursor-pointer stroke-light hover:stroke-dark"
+          onClick={() => alert('click')}
+        />
+        <div
+          className="font-bold bg-primary-dark py-2 px-3 text-white cursor-pointer"
+          onClick={() => nav('/')}
+        >
+          PETIONARY.
+        </div>
       </section>
       <section className="flex items-center justify-between w-[30rem]">
-        <Input width="md" rounded />
-        <div className="flex w-[8rem] justify-between">
-          <span className="p-2">
-            <MessageIcon />
-          </span>
-          <span className="p-2">
-            <UserIcon />
-          </span>
-          <span className="p-2">
-            <LogoutIcon />
-          </span>
-        </div>
+        <Input width="md" placeholder="검색어를 입력해 주세요." rounded />
+        <Icons
+          handleLogout={() => alert('log out')}
+          handleToMessage={() => alert('message')}
+          handleToMypage={() => alert('mypage')}
+        />
       </section>
     </header>
   );
