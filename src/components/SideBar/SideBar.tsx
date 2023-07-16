95c3dac53ae935bb87fd76487ee465c1e5baa2da
cos4338@gmail.com <cos4338@gmail.com>
2023-07-17 00:09:13 +0900

    design : modify z-index of SideBar component - SideBar z-index 수정

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -6,12 +6,10 @@ interface SideBarProps {
 }
 
 const SideBar = ({ menuToggle }: SideBarProps) => {
-  const isOpen = menuToggle
-    ? 'lg:translate-x-0 lg:m-0 translate-x-0 m-auto'
-    : '-translate-x-[110%] m-0';
+  const isOpen = menuToggle ? 'lg:translate-x-0 lg:m-0 m-auto' : '-translate-x-[110%] m-0';
   return (
     <aside
-      className={`transition none w-[15rem] h-[90vh] overflow-y-auto p-5 flex flex-col justify-between ease-in-out duration-500 ${isOpen} bg-transparent`}
+      className={`sm:w-full w-[15rem] h-[90vh] overflow-y-auto p-5 flex flex-col justify-between duration-500 ${isOpen} bg-[#fff] absolute z-20`}
     >
       <MenuList />
       <UserMenu />
