c501dbfe0683613657e0d587873148123abe11e3
cos4338@gmail.com <cos4338@gmail.com>
2023-07-19 22:11:52 +0900

    fix : fix eslint error

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -3,7 +3,6 @@ import { ChangeEvent, useState } from 'react';
 const useInput = <T>(initialInput: T) => {
   const [input, setInput] = useState(initialInput);
 
-  // eslint-disable-next-line no-unused-vars
   const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value });
