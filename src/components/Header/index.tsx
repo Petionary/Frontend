6440dd3ed70a43ff1b7e6cd51116e15cdc6bbda0
cos4338@gmail.com <cos4338@gmail.com>
2023-08-02 18:25:25 +0900

    style : 테일윈드 색상 설정 변경 및 로고 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import SideBar from '../SideBar';
 import Input from '../Input';
 
 import Icons from './components/Icons';
-import HeaderLogo from './components/HeaderLogo';
+import { ReactComponent as LogoBanner } from '../../assets/Logo_Banner.svg';
 import HeaderMenuToggle from './components/HeaderMenuToggle';
 import useToggle from '../../hooks/useToggle';
 
@@ -20,7 +20,7 @@ const Header = () => {
       <header className="flex justify-between px-10 w-full h-[4rem]">
         <section className="flex items-center w-[10rem] justify-between sm:w-full">
           <HeaderMenuToggle menuToggle={menuToggle} handleMenuOpen={handleMenuToggle} />
-          <HeaderLogo onClick={navHome} />
+          <LogoBanner onClick={navHome} />
         </section>
         <section className="flex items-center justify-between w-[30rem] sm:hidden">
           <Input width="md" placeholder="검색어를 입력해 주세요." rounded />
