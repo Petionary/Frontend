60f2064d95d06b86afec649d16484f27405508cf
cos4338@gmail.com <cos4338@gmail.com>
2023-08-03 20:27:20 +0900

    style : 헤더 컴포넌트 UI 수정

--- src/components/Header/components/BottomHeader/index.tsx
+++ src/components/Header/components/BottomHeader/index.tsx
@@ -1,4 +1,5 @@
-import HeaderMenuToggle from '../HeaderMenuToggle';
+import HeaderMenuToggle from './components/HeaderMenuToggle';
+import NavBar from './components/NavBar';
 
 interface BottomHeaderProps {
   menuToggle: boolean;
@@ -7,22 +8,11 @@ interface BottomHeaderProps {
 
 const BottomHeader = ({ menuToggle, handleMenuToggle }: BottomHeaderProps) => {
   return (
-    <section className="w-full items-center flex justify-between text-xs text-gray-100 border-t border-b h-[3.5rem] px-[10rem]">
+    <section className="w-full items-center flex justify-between text-xs text-gray-100 border-t border-b h-[3.5rem] px-[10rem] sm:px-10">
       <div className="flex items-center w-[30rem] justify-between">
-        <div
-          className="flex items-center justify-between w-[4rem] cursor-pointer"
-          onClick={handleMenuToggle}
-        >
-          <HeaderMenuToggle menuToggle={menuToggle} handleMenuOpen={handleMenuToggle} />
-          <span>카테고리</span>
-        </div>
-        <nav className="flex items-center w-[15rem] justify-between">
-          <span>Place</span>
-          <span>Community</span>
-          <span>Report</span>
-        </nav>
+        <HeaderMenuToggle menuToggle={menuToggle} onClick={handleMenuToggle} />
+        <NavBar />
       </div>
-
       <div className="flex items-center w-[7rem] justify-between text-gray-70">
         <span>로그인</span>
         <span>회원가입</span>
