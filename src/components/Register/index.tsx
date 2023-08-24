a24b02ba35871725c5cb2429b416bf7fff3cc3ec
cos4338@gmail.com <cos4338@gmail.com>
2023-08-24 20:50:53 +0900

    feat : 회원정보 입력 페이지 타이틀 UI 구현 - 프로세스 단계에 따른 타이틀 렌더링

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,43 +1,50 @@
 import Input from '../Input';
 import useInput from '../../hooks/useInput';
 import Button from '../Button';
+import { useState } from 'react';
+import UserInfoTitle from './components/UserInfoTitle';
 
 const Register = () => {
-  const { input, handleInputChange } = useInput({ nickname: '' });
+  const { input, handleInputChange } = useInput({ nickname: '', places: [] });
+  const [step, setStep] = useState(0);
+
+  const handlePrevStep = () => {
+    setStep(step - 1);
+  };
+
+  const handleNextStep = () => {
+    setStep(step + 1);
+  };
 
   return (
-    <dialog open className="w-[50.8125rem] h-[46.875rem] p-0 flex overflow-hidden top-[10.31rem]">
+    <dialog
+      open
+      className="w-[50.8125rem] h-[46.875rem] p-0 flex overflow-hidden top-[10.31rem] border"
+    >
       {/* 배경 이미지 */}
       <section className="flex w-[25.8125rem] h-full overflow-hidden">
-        <img
-          src="src/assets/bg-image.jpeg"
-          alt="background image"
-          className="absolute w-[60.1875rem] h-[46.9rem]"
-        />
+        <img src="" alt="background image" className="absolute w-[60.1875rem] h-[46.9rem] bg-red" />
         {/* process */}
       </section>
       {/* 1. 별명 입력 -> 2. 장소 추가 -> 3. 반려동물 프로필 등록 */}
       {/* 별명 입력 */}
       <section className="flex flex-col justify-between items-center w-[25rem] h-full z-10 bg-white pt-[3.75rem] pb-[2.62rem]">
-        <div>
-          <h1 className="text-[1.25rem] text-gray-100 font-[600]">
-            반가워요! 어떻게 불러 드리면 될까요?
-          </h1>
-          <p className="text-[0.75rem] text-gray-50">마이페이지에서 언제든지 수정 가능해요.</p>
-        </div>
-        <form>
-          <Input
-            label="닉네임"
-            width="sm"
-            name="nickname"
-            value={input.nickname}
-            onChange={handleInputChange}
-            isValid={input.nickname?.length > 0}
-          />
-        </form>
+        <UserInfoTitle step={step} />
+        <Input
+          label="닉네임"
+          width="sm"
+          name="nickname"
+          value={input.nickname}
+          onChange={handleInputChange}
+          isValid={input.nickname?.length > 0}
+        />
         <div className="w-full px-[5rem] flex justify-between">
-          <Button disabled>Back</Button>
-          <Button cta>Next</Button>
+          <Button onClick={handlePrevStep} disabled={step === 0}>
+            Back
+          </Button>
+          <Button cta onClick={handleNextStep}>
+            Next
+          </Button>
         </div>
       </section>
     </dialog>
