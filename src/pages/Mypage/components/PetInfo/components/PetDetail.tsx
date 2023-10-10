3005ad5229f2fa75b978f92e4f901a425614c514
cos4338@gmail.com <cos4338@gmail.com>
2023-10-10 15:34:21 +0900

    fix : fix lint error

--- src/pages/Mypage/components/PetInfo/components/PetDetail.tsx
+++ src/pages/Mypage/components/PetInfo/components/PetDetail.tsx
@@ -6,7 +6,7 @@ interface PetDetailProps {
   petId: number | undefined;
 }
 
-const PetDetail = ({ petId }: PetDetailProps) => {
+const PetDetail = ({}: PetDetailProps) => {
   // prop으로 전달받은 id에 맞는 데이터 fetch
   // id === undefined => pet 추가 페이지 보여주기
   const [editToggle, handleEditToggle] = useToggle();
