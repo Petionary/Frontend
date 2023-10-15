30ea152f8c375643738326c3981f7d41a9cc1841
cos4338@gmail.com <cos4338@gmail.com>
2023-10-15 20:40:12 +0900

    feat : 리뷰작성 모달창 오픈 구현

--- src/pages/PlaceDetail/components/ReviewContainer/components/ReviewHeader/components/ReviewSortItem.tsx
+++ src/pages/PlaceDetail/components/ReviewContainer/components/ReviewHeader/components/ReviewSortItem.tsx
@@ -10,10 +10,12 @@ const ReviewSortItem = ({ rate_order, ...rest }: ReviewSortItemProps) => {
     rate: rate_order === 'desc' ? '평점 높은순' : '평점 낮은순',
   };
 
-  const isChecked = rest.checked ? 'text-primary border-primary' : 'border-white text-gray-100';
+  const isChecked = rest.checked
+    ? 'text-primary border-primary'
+    : 'border-white text-gray-100 hover:text-primary hover:border-primary';
 
   return (
-    <label className="mx-[0.75rem]">
+    <label className="mx-[0.75rem] cursor-pointer">
       <input type="checkbox" className="sr-only peer" {...rest} />
       <p
         className={`${isChecked} py-[0.625rem] px-[1rem] border rounded-[0.25rem] text-[0.9375rem] font-[400] -tracking-[0.00469rem]`}
--- src/pages/PlaceDetail/components/ReviewContainer/index.tsx
+++ src/pages/PlaceDetail/components/ReviewContainer/index.tsx
@@ -2,6 +2,8 @@ import useInput from '../../../../hooks/useInput';
 import ReviewHeader from './components/ReviewHeader';
 import CreateReviewButton from './components/CreateReviewButton';
 import useToggle from '../../../../hooks/useToggle';
+import useScrollLock from '../../../../hooks/useScrollLock';
+import { useEffect } from 'react';
 
 const ReviewContainer = () => {
   const { input, handleInputChange, setInput } = useInput<{ sort: string; order: 'asc' | 'desc' }>({
@@ -10,9 +12,15 @@ const ReviewContainer = () => {
   });
 
   const [createReviewToggle, hanndleCreateReviewToggle] = useToggle();
+  const { lock, unlock } = useScrollLock();
+
+  useEffect(() => {
+    if (createReviewToggle) lock();
+    else unlock();
+  });
 
   return (
-    <section className="w-full px-[16.67vw] relative">
+    <section className="w-full px-[16.67vw]">
       <ReviewHeader
         reviewSortParams={input}
         onChangeReviewSortParams={handleInputChange}
@@ -22,9 +30,12 @@ const ReviewContainer = () => {
         createReviewToggle={createReviewToggle}
         handleCreateReviewToggle={hanndleCreateReviewToggle}
       />
+      {createReviewToggle && (
+        <div className="w-[100vw] h-[100vh] bg-gray-100 absolute top-0 left-0 opacity-[0.5]" />
+      )}
       <dialog
         open={createReviewToggle}
-        className="w-[52.875rem] h-[51.8125rem] bg-background absolute"
+        className="w-[52.875rem] h-[85vh] bg-background absolute top-[6vh]"
       >
         open
       </dialog>
